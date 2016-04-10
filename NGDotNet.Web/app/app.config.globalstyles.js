10c5ffac020ff134271e72a8231b086069d34c61
Ashlin Allen <ashlin.allen@gmail.com>
2016-04-09 21:52:30 -0700

    Minor tweak to comment in app.config.globalstyles.js for clarity

--- NGDotNet.Web/app/app.config.globalstyles.js
+++ NGDotNet.Web/app/app.config.globalstyles.js
@@ -1,10 +1,10 @@
 /* Require global CSS here so Webpack knows we need the files. */
 
-/* Vendor styles */
+/* Vendor styles required from NPM */
 require('toastr/toastr.scss');
 require('bootstrap/dist/css/bootstrap.css');
 require('font-awesome/css/font-awesome.css');
 
-/* Global site styles */
+/* Global site styles required from our repo */
 require('./assets/Styles/customtheme.scss');
 require('./assets/Styles/styles.scss');
